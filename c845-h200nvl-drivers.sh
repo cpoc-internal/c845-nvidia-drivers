f4c865adec4d40cb936c219a60c18dd978dff0e9
earguedast <este.at@gmail.com>
2026-04-24 15:34:18 -0600

    Create c845-h200nvl-drivers.sh

---
+++ c845-h200nvl-drivers.sh
@@ -0,0 +1,47 @@
+
+echo "Setting up the Script Variables..."
+set -o nounset
+disable_apparmor=true
+disable_firewall=true
+
+# Disable AppArmor
+if [ "$disable_apparmor" = "true" ]; then
+    echo "Disabling AppArmor..."
+    sudo systemctl stop apparmor
+    sudo systemctl disable apparmor
+fi
+
+# Disable Firewall
+if [ "$disable_firewall" = "true" ]; then
+    echo "Disabling the Firewall..."
+    sudo systemctl stop ufw
+    sudo systemctl disable ufw
+fi
+
+sudo apt install build-essential linux-headers-$(uname -r) -y
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
+sudo apt update
+sudo apt install nvidia-driver-580-server -y
+sudo apt-get update
+sudo apt-get install -y nvidia-fabricmanager-580=580.126.09-0ubuntu0.22.04.1
+sudo systemctl enable nvidia-fabricmanager
+sudo systemctl start nvidia-fabricmanager
+sudo nvidia-smi -pm 1
+sudo modprobe nvidia-uvm
+sudo modprobe nvidia-modeset
+sudo apt-get install -y cuda-toolkit-13-0 -y
+sudo nvidia-smi
+sudo apt install nvidia-cuda-toolkit -y
+export CUDA_HOME=/usr/local/cuda-13.0
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+nvcc --version
+
+sudo apt install lldpd -y
+sudo systemctl start lldpd
+sudo systemctl enable lldpd
+sudo systemctl status lldpd
+
+
+sudo systemctl status nvidia-fabricmanager
